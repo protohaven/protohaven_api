2f9c8f93264c075268881a23b4273ec968122583
Scott Martin <smartin015@gmail.com>
2023-12-06 09:25:53 -0500

    Update class selector link and jsonify data

--- main.py
+++ main.py
@@ -48,9 +48,10 @@ def user_fullname():
 def index():
     neon_account = session.get('neon_account')
     neon_account['custom_fields'] = {}
+    neon_json = json.dumps(neon_account, indent=2)
     for cf in neon_account['individualAccount']['accountCustomFields']:
         neon_account['custom_fields'][cf['name']] = cf
-    return render_template("dashboard.html", fullname=user_fullname(), email=user_email(), neon_id=session.get('neon_id'), neon_account=neon_account)
+    return render_template("dashboard.html", fullname=user_fullname(), email=user_email(), neon_id=session.get('neon_id'), neon_account=neon_account, neon_json=neon_json)
 
 @app.route("/login")
 def login_user_neon_oauth():
