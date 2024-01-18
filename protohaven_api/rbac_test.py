38ab893fc8d2457b32d77f79537bc8ce47444f27
Scott Martin <smartin015@gmail.com>
2024-01-18 14:33:02 -0500

    Add initial unit testing

--- protohaven_api/rbac_test.py
+++ protohaven_api/rbac_test.py
@@ -1,16 +1,9 @@
 """Unit tests for role-based authentication"""
-import unittest
-
 from protohaven_api import rbac
 
-# from unittest.mock import patch
 
-
-class RBACTest(unittest.TestCase):
-    """Tests for role based authentication"""
-
-    def test_require_login_redirect(self):
-        """require_login() redirects when not logged in"""
-        fn = rbac.require_login(lambda: "called")
-        result = fn()
-        self.assertTrue(result)
+def test_require_login_redirect(self):
+    """require_login() redirects when not logged in"""
+    fn = rbac.require_login(lambda: "called")
+    result = fn()
+    self.assertTrue(result)
