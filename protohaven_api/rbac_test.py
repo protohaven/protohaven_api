8184c4cf1ac3df875713ea228e0517640ff5adf1
Scott Martin <smartin015@gmail.com>
2024-01-18 15:03:21 -0500

    Fix test

--- protohaven_api/rbac_test.py
+++ protohaven_api/rbac_test.py
@@ -1,9 +1,24 @@
 """Unit tests for role-based authentication"""
+from unittest.mock import MagicMock
+
 from protohaven_api import rbac
 
 
-def test_require_login_redirect(self):
+def test_require_login_redirect(monkeypatch):
     """require_login() redirects when not logged in"""
+    monkeypatch.setattr("protohaven_api.rbac.session", {})
+    monkeypatch.setattr("protohaven_api.rbac.request", MagicMock(url="Testurl"))
+    monkeypatch.setattr("protohaven_api.rbac.redirect", lambda x: x)
+    monkeypatch.setattr("protohaven_api.rbac.url_for", lambda x: x)
+
     fn = rbac.require_login(lambda: "called")
     result = fn()
-    self.assertTrue(result)
+    assert result == "login_user_neon_oauth"
+
+
+def test_require_login_ok(monkeypatch):
+    """require_login() executes function when logged in"""
+    monkeypatch.setattr("protohaven_api.rbac.session", {"neon_id": "foo"})
+    fn = rbac.require_login(lambda: "called")
+    result = fn()
+    assert result == "called"
