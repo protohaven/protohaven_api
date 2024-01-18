51d9006b523f53033054669fe00861545b93500e
Scott Martin <smartin015@gmail.com>
2024-01-18 15:18:31 -0500

    Lazy load config

--- protohaven_api/config.py
+++ protohaven_api/config.py
@@ -1,10 +1,12 @@
 """Loads config.yaml"""
+from functools import cache
+
 import yaml
 
-with open("config.yaml", "r", encoding="utf-8") as f:
-    cfg = yaml.safe_load(f)
 
-
+@cache
 def get_config():
     """Fetches the config"""
+    with open("config.yaml", "r", encoding="utf-8") as f:
+        cfg = yaml.safe_load(f)
     return cfg
