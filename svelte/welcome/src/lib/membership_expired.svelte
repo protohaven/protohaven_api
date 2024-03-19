2f2ae261024020eefbb4bb18ea4445855bf95d92
Scott Martin <smartin015@gmail.com>
2024-03-19 11:44:47 -0400

    Add missing lib/ dir in svelte app

---
+++ svelte/welcome/src/lib/membership_expired.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Alert, Button, Row, Col } from '@sveltestrap/sveltestrap';
+  export let on_close;
+  export let name;
+</script>
+
+<Alert color="danger" fade={false} open={true}>
+  <Row class="text-center justify-content-center">
+    <h1>{name}, please see a tech on duty.</h1>
+    <h3>Your membership is not listed as active.</h3>
+    <p>See the whiteboard by the front desk for current on-duty techs.</p>
+  </Row>
+
+  <Row class="justify-content-center">
+    <Col sm={{size: 'auto'}} class="my-3">
+    <Button color="danger" on:click={on_close}>Done</Button>
+    </Col>
+  </Row>
+</Alert>
