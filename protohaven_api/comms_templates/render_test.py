3aab34d2ed1740e04198bf0d5760b195f3e20e66
Scott Martin <smartin015@gmail.com>
2024-10-04 16:56:50 -0400

    Remove testfiles in favor of hash strategy

--- protohaven_api/comms_templates/render_test.py
+++ protohaven_api/comms_templates/render_test.py
@@ -1,13 +1,10 @@
 """Unit tests for comms_templates module"""
-import os
-from pathlib import Path
+import hashlib
 
 import pytest
 
 from protohaven_api.comms_templates import get_all_templates, render
 from protohaven_api.testing import d
-
-WANTFILE_DIR = Path(__file__).parent / "testdata/"
 
 
 def test_comms_render():
@@ -27,15 +24,6 @@ def test_templates_html_detection():
     assert got
 
 
-def load_wantfile(name):
-    """Loads a file representing expected outcome of running the test"""
-    with open(os.path.join(WANTFILE_DIR, f"{name}.txt"), "r", encoding="utf8") as file:
-        data = file.read().strip()
-    lines = data.splitlines()
-    assert len(lines) >= 3
-    return lines[1].strip(), "\n".join(lines[2:]).strip(), lines[0] == "True"
-
-
 TEST_EVENT = {
     "id": "34567",
     "python_date": d(0),
@@ -49,12 +37,11 @@ TEST_ATTENDEE = {
     "email": "test@attendee.com",
 }
 TESTED_TEMPLATES = [
-    ("test_template", {"val": "test_body"}, "test_template"),
-    ("test_html_template", {"val": "test_body"}, "test_html_template"),
+    ("test_template", {"val": "test_body"}),
+    ("test_html_template", {"val": "test_body"}),
     (
         "admin_create_suspension",
         {"neon_id": 12345, "end": d(0)},
-        "admin_create_suspension",
     ),
     (
         "class_automation_summary",
@@ -67,23 +54,19 @@ TESTED_TEMPLATES = [
                 },
             }
         },
-        "class_automation_summary",
     ),
-    ("class_proposals", {"unapproved": ["Unap1", "Unap2"]}, "class_proposals"),
+    ("class_proposals", {"unapproved": ["Unap1", "Unap2"]}),
     (
         "class_scheduled",
         {"inst": "InstName", "formatted": ["Class1", "Class2"], "n": 2},
-        "class_scheduled",
     ),
     (
         "daily_private_instruction",
         {"formatted": ["Req 1", "Req 2"]},
-        "daily_private_instruction",
     ),
     (
         "discord_nick_change_summary",
         {"n": 2, "changes": ["Name1", "Name2"], "m": 1, "notified": ["testuser"]},
-        "discord_nick_change_summary",
     ),
     (
         "discord_role_change_dm",
@@ -92,7 +75,6 @@ TESTED_TEMPLATES = [
             "not_associated": True,
             "discord_id": "testid",
         },
-        "discord_role_change_dm",
     ),
     (
         "discord_role_change_summary",
@@ -103,7 +85,6 @@ TESTED_TEMPLATES = [
             "roles_revoked": 3,
             "footer": "Test Footer",
         },
-        "discord_role_change_summary",
     ),
     (
         "enforcement_summary",
@@ -111,7 +92,6 @@ TESTED_TEMPLATES = [
             "vs": [{"onset": d(0), "fee": 5, "unpaid": 10, "notes": "Test violation"}],
             "ss": [{"start": d(0), "end": d(1)}],
         },
-        "enforcement_summary",
     ),
     (
         "init_membership",
@@ -121,64 +101,53 @@ TESTED_TEMPLATES = [
             "coupon_amount": 5,
             "sample_classes": ["class1", "class2"],
         },
-        "init_membership",
     ),
     (
         "instruction_requests",
         {"num": 3, "formatted": ["req 1", "req 2", "req 3"]},
-        "instruction_requests",
     ),
     (
         "instructor_applications",
         {"open_applicants": ["Foo", "Bar", "Baz"]},
-        "instructor_applications",
     ),
-    ("instructor_check_supplies", {"evt": TEST_EVENT}, "instructor_check_supplies"),
-    ("instructor_class_canceled", {"evt": TEST_EVENT}, "instructor_class_canceled"),
-    ("instructor_class_confirmed", {"evt": TEST_EVENT}, "instructor_class_confirmed"),
-    ("instructor_log_reminder", {"evt": TEST_EVENT}, "instructor_log_reminder"),
-    ("instructor_low_attendance", {"evt": TEST_EVENT}, "instructor_low_attendance"),
+    ("instructor_check_supplies", {"evt": TEST_EVENT}),
+    ("instructor_class_canceled", {"evt": TEST_EVENT}),
+    ("instructor_class_confirmed", {"evt": TEST_EVENT}),
+    ("instructor_log_reminder", {"evt": TEST_EVENT}),
+    ("instructor_low_attendance", {"evt": TEST_EVENT}),
     (
         "instructor_schedule_classes",
         {"firstname": "Firstname", "start": d(0), "end": d(30)},
-        "instructor_schedule_classes",
     ),
     (
         "instructors_new_classes",
         {"formatted": ["a", "b", "c"], "n": 3},
-        "instructors_new_classes",
     ),
     (
         "membership_validation_problems",
         {"problems": ["Problem 1", "Problem 2"]},
-        "membership_validation_problems",
     ),
-    ("new_project_request", {"notes": "test_notes"}, "new_project_request"),
-    ("not_associated", {"discord_id": "testid"}, "not_associated"),
+    ("new_project_request", {"notes": "test_notes"}),
+    ("not_associated", {"discord_id": "testid"}),
     (
         "phone_message",
         {"msg_header": "stuff and things", "notes": "This is a message", "date": d(0)},
-        "phone_message",
     ),
     (
         "registrant_class_canceled",
         {"evt": TEST_EVENT, "a": TEST_ATTENDEE},
-        "registrant_class_canceled",
     ),
     (
         "registrant_class_confirmed",
         {"evt": TEST_EVENT, "a": TEST_ATTENDEE, "now": d(-1)},
-        "registrant_class_confirmed",
     ),
     (
         "registrant_post_class_survey",
         {"evt": TEST_EVENT, "a": TEST_ATTENDEE},
-        "registrant_post_class_survey",
     ),
     (
         "schedule_push_notification",
         {"title": "TestTitle", "formatted": ["a", "b", "c"]},
-        "schedule_push_notification",
     ),
     (
         "shift_no_techs",
@@ -186,12 +155,10 @@ TESTED_TEMPLATES = [
             "shift": "Monwednesaturday TM",
             "onduty": [("Tech A", "a@a.com"), ("Tech B", "b@b.com")],
         },
-        "shift_no_techs",
     ),
     (
         "shop_tech_applications",
         {"open_applicants": ["Foo", "Bar", "Baz"]},
-        "shop_tech_applications",
     ),
     (
         "square_validation_action_needed",
@@ -199,9 +166,8 @@ TESTED_TEMPLATES = [
             "unpaid": ["a", "b"],
             "untaxed": ["c", "d"],
         },
-        "square_validation_action_needed",
     ),
-    ("suspension_ended", {"firstname": "Firstname"}, "suspension_ended"),
+    ("suspension_ended", {"firstname": "Firstname"}),
     (
         "suspension_started",
         {
@@ -210,7 +176,6 @@ TESTED_TEMPLATES = [
             "start": d(0),
             "accrued": 100,
         },
-        "suspension_started",
     ),
     (
         "tech_daily_tasks",
@@ -220,7 +185,6 @@ TESTED_TEMPLATES = [
             "new_tasks": [{"name": "Test Task", "gid": "123"}],
             "closing": "Stay tested!",
         },
-        "tech_daily_tasks",
     ),
     (
         "tech_leads_maintenance_status",
@@ -229,13 +193,11 @@ TESTED_TEMPLATES = [
             "stale_thresh": 999,
             "stale_tasks": [{"name": "Test Task", "gid": "123", "days_ago": 9001}],
         },
-        "tech_leads_maintenance_status",
     ),
-    ("tech_openings", {"n": 1, "events": [TEST_EVENT]}, "tech_openings"),
+    ("tech_openings", {"n": 1, "events": [TEST_EVENT]}),
     (
         "tool_documentation",
         {"n": 1, "tool_tutorials": "tutorial info", "clearance_docs": "clearance info"},
-        "tool_documentation",
     ),
     (
         "violation_ongoing",
@@ -246,7 +208,6 @@ TESTED_TEMPLATES = [
             "notes": "Detailed violation notes",
             "accrued": 10,
         },
-        "violation_ongoing",
     ),
     (
         "violation_started",
@@ -257,23 +218,69 @@ TESTED_TEMPLATES = [
             "sections": ["Section A", "Section B"],
             "fee": 5,
         },
-        "violation_started",
     ),
 ]
+HASHES = {
+    "test_template": "b8a27190aa3ed922",
+    "test_html_template": "77606b5538c73e78",
+    "admin_create_suspension": "b43c46a0f86ee0d7",
+    "class_automation_summary": "866427c2de1c186f",
+    "class_proposals": "1d1bef17435d88da",
+    "class_scheduled": "d7638c67655ae1eb",
+    "daily_private_instruction": "ba81765c045917ee",
+    "discord_nick_change_summary": "88493fe928a1f0d4",
+    "discord_role_change_dm": "e36c6c70681a8804",
+    "discord_role_change_summary": "8a34d924f30d0625",
+    "enforcement_summary": "53728e884f150eec",
+    "init_membership": "8c33f4e841e2b782",
+    "instruction_requests": "7e0902003add426d",
+    "instructor_applications": "282f1d709883f273",
+    "instructor_check_supplies": "73815da04e9f47cc",
+    "instructor_class_canceled": "57dc5ce8d4ec5317",
+    "instructor_class_confirmed": "51392e2fb41f37f8",
+    "instructor_log_reminder": "d00bf87676ad240e",
+    "instructor_low_attendance": "e7b4548a7a3f7fc2",
+    "instructor_schedule_classes": "39aea10c71fc8895",
+    "instructors_new_classes": "c3cbf1129a256abe",
+    "membership_validation_problems": "959c9f70f5ab648e",
+    "new_project_request": "4cffeae1816d93a2",
+    "not_associated": "a8bd9d0037db82a5",
+    "phone_message": "c17d7359c5ddace4",
+    "registrant_class_canceled": "a3b36f01fde3ee4c",
+    "registrant_class_confirmed": "38e0456bdb64d363",
+    "registrant_post_class_survey": "7f89d4a2a4211f67",
+    "schedule_push_notification": "78908794e790a632",
+    "shift_no_techs": "9a2c858ff7ac2456",
+    "shop_tech_applications": "cd77b6978d522da3",
+    "square_validation_action_needed": "3ed4e73c9efa37db",
+    "suspension_ended": "d05255f2fcb8e992",
+    "suspension_started": "3b211a9a7c04be8a",
+    "tech_daily_tasks": "55af86e04ce2551c",
+    "tech_leads_maintenance_status": "e763f572fa0203a5",
+    "tech_openings": "f9bd7999e37d1ebd",
+    "tool_documentation": "ea11c1bf9a119b05",
+    "violation_ongoing": "1ff24f039d2d424a",
+    "violation_started": "12527581a8fbdd2d",
+}
 
 
-@pytest.mark.parametrize("template_name, kwargs, wantfile", TESTED_TEMPLATES)
-def test_template_rendering(template_name, kwargs, wantfile):
+def _gethash(data):
+    h = hashlib.sha256()
+    h.update(bytes(f"{data}", encoding="utf8"))
+    return h.hexdigest()[:16]
+
+
+@pytest.mark.parametrize("template_name, kwargs", TESTED_TEMPLATES)
+def test_template_rendering(template_name, kwargs):
     """Test jinja templates with dummy arguments, ensuring they match
-    the wantfiles"""
+    the prior hash"""
     got = render(template_name, **kwargs)
-    want = load_wantfile(wantfile)
-    if got != want:
+    gothash = _gethash(got)
+    wanthash = HASHES[template_name]
+    if gothash != wanthash:
         raise RuntimeError(
-            f"Mismatch between render of template {template_name}(**{kwargs}) and "
-            f"test file {wantfile}:\n\n==== WANTED ===\n{want}\n==== GOT ===\n{got}\n\n"
-            "To regenerate all test files, run python3 -m "
-            "protohaven_api.comms_templates.render_test"
+            f"Test output hash {gothash} does not match prior hash {wanthash}:\n"
+            "got[0]\ngot[1]\ngot[2]"
         )
     assert got[0] != got[1]  # Subject should never match body
 
@@ -285,20 +292,8 @@ def test_all_templates_tested(tmpl):
     assert tmpl in tested_templates
 
 
-def rerender_wantfiles():
-    """Render templates with the test kwargs into files in testdata/"""
-    for template_name, kwargs, wantfile in TESTED_TEMPLATES:
-        with open(
-            os.path.join(WANTFILE_DIR, f"{wantfile}.txt"), "w", encoding="utf8"
-        ) as file:
-            subject, body, is_html = render(template_name, **kwargs)
-            file.write(f"{is_html}\n{subject}\n{body}")
-            print(f"Wrote {wantfile}.txt")
-    print("Done")
-
-
 if __name__ == "__main__":
-    input(
-        "Press Enter to render all templates into testdata/. This will overwrite file data!"
-    )
-    rerender_wantfiles()
+    hashes = {}
+    for i, ttt in enumerate(TESTED_TEMPLATES):
+        hashes[ttt[0]] = _gethash(render(*ttt))
+    print(hashes)
