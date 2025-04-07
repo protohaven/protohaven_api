5213d8fd76e28d8e1ed9c77ae67426cfd9801612
Scott Martin <smartin015@gmail.com>
2025-04-07 11:26:34 -0400

    Fix tuple issue in get_all_records_between, test

--- protohaven_api/integrations/airtable_base_test.py
+++ protohaven_api/integrations/airtable_base_test.py
@@ -2,6 +2,7 @@
 import json
 
 from protohaven_api.integrations import airtable_base as a
+from protohaven_api.testing import d
 
 
 def test_get_all_records_airtable(mocker):
@@ -23,6 +24,22 @@ def test_get_all_records_airtable(mocker):
 
     _, args, kwargs = a.get_connector().db_request.mock_calls[1]
     assert kwargs["suffix"] == "?offset=1&a=test_suffix"
+
+
+def test_get_all_records_between_airtable(mocker):
+    mocker.patch.object(a, "get_connector")
+    a.get_connector().db_format.return_value = "airtable"
+    a.get_connector().db_request.side_effect = [
+        (200, {"records": ["foo", "bar", "baz"]}),
+    ]
+    assert a.get_all_records_between("test_base", "test_tbl", d(0), d(1)) == [
+        "foo",
+        "bar",
+        "baz",
+    ]
+    _, args, kwargs = a.get_connector().db_request.mock_calls[0]
+    assert d(0).isoformat() in kwargs["suffix"]
+    assert d(1).isoformat() in kwargs["suffix"]
 
 
 def test_get_all_records_nocodb(mocker):
@@ -57,3 +74,24 @@ def test_get_all_records_nocodb(mocker):
     _, args, kwargs = a.get_connector().db_request.mock_calls[1]
     print(kwargs)
     assert kwargs["suffix"] == "?offset=5&a=test_suffix"
+
+
+def test_get_all_records_between_nocodb(mocker):
+    mocker.patch.object(a, "get_connector")
+    a.get_connector().db_format.return_value = "nocodb"
+    rec = {"Id": "id1", "a": 5}
+    a.get_connector().db_request.side_effect = [
+        (
+            200,
+            {
+                "list": [rec],
+                "pageInfo": {"isLastPage": True},
+            },
+        ),
+    ]
+    assert a.get_all_records_between("test_base", "test_tbl", d(0), d(1)) == [
+        {"id": rec["Id"], "fields": rec}
+    ]
+    _, args, kwargs = a.get_connector().db_request.mock_calls[0]
+    assert d(0).isoformat() in kwargs["suffix"]
+    assert d(1).isoformat() in kwargs["suffix"]
