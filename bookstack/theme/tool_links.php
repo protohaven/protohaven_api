fa0fa493d05cb05c49526990a8d4c6aa24500948
Scott Martin <smartin015@gmail.com>
2025-04-28 20:08:28 -0400

    Harden tool docs linker, add summary url (#295)

--- bookstack/theme/tool_links.php
+++ bookstack/theme/tool_links.php
@@ -10,12 +10,24 @@ use BookStack\Activity\Models\Tag;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
+function getPageCategory($page) {
+  $CLEARANCE_BOOKS = explode(',', getenv('PH_TOOL_LINK_CLEARANCE_BOOKS') ?: "clearances");
+  $TOOL_TUTORIAL_BOOKS = explode(',', getenv('PH_TOOL_LINK_TUTORIAL_BOOKS') ?: "tool-guides");
+  $book_slug = $page['book']['slug'];
+  if (in_array($book_slug, $CLEARANCE_BOOKS)) {
+    return "clearance";
+  } elseif (in_array($book_slug, $TOOL_TUTORIAL_BOOKS)) {
+    return "tool_tutorial";
+  } else {
+    return "other";
+  }
+}
 
 function getPageURLsWithToolCode($tool_code) {
-  $CLEARANCE_BOOKS = ["clearances"];
-  $TOOL_TUTORIAL_BOOKS = ["tool-guides"];
   $tags = Tag::where('name', 'tool_code')
-    ->where('value', $tool_code)
+    # Note that this variable binding use of whereRaw prevents
+    # sql injection - see https://stackoverflow.com/a/78014344
+    ->whereRaw('LOWER(value) = ?', [trim(strtolower($tool_code))])
     ->with('entity')
     ->with('entity.book')
     ->get();
@@ -23,13 +35,8 @@ function getPageURLsWithToolCode($tool_code) {
   foreach ($tags as $tag) {
     // Log::info($tag);
     $page = $tag->entity ?? null;
-    $book_slug = $page['book']['slug'];
     if ($page instanceof \BookStack\Entities\Models\Page) {
-      if (in_array($book_slug, $CLEARANCE_BOOKS)) {
-        $result["clearance"] = $page->getUrl();
-      } elseif (in_array($book_slug, $TOOL_TUTORIAL_BOOKS)) {
-        $result["tool_tutorial"] = $page->getUrl();
-      }
+      $result[getPageCategory($page)] = $page->getUrl();
     }
   }
   return $result;
@@ -39,15 +46,32 @@ function getPageURLsWithToolCode($tool_code) {
 Theme::listen(ThemeEvents::ROUTES_REGISTER_WEB, function (Router $router) {
 
   $router->get('/tool_clearance/{tool_code}', function ($tool_code) {
-    $NOTFOUND_URL = "/books/clearances/page/missing-clearance-page";
+    $NOTFOUND_URL = getenv("PH_TOOL_LINK_CLEARANCE_NOT_FOUND_URL") ?: "/books/clearances/page/missing-clearance-page";
     $urls = getPageURLsWithToolCode($tool_code);
     return redirect($urls['clearance'] ?? $NOTFOUND_URL);
   });
 
   $router->get('/tool_tutorial/{tool_code}', function ($tool_code) {
-    $NOTFOUND_URL = "/books/tool-guides/page/tool-guide-missing";
+    $NOTFOUND_URL = getenv("PH_TOOL_LINK_TUTORIAL_NOT_FOUND_URL") ?: "/books/tool-guides/page/tool-guide-missing";
     $urls = getPageURLsWithToolCode($tool_code);
     return redirect($urls['tool_tutorial'] ?? $NOTFOUND_URL);
+  });
+
+  $router->get('/tool_docs_report', function() {
+    $tags = Tag::where('name', 'tool_code')
+      ->whereNotNull('value')
+      ->with('entity')
+      ->with('entity.book')
+      ->get();
+    $result = array();
+    foreach ($tags as $tag) {
+      $page = $tag->entity ?? null;
+      $book_slug = $page['book']['slug'];
+      if ($page instanceof \BookStack\Entities\Models\Page) {
+        $result[strtoupper($tag["value"])][getPageCategory($page)][] = $page->getUrl();
+      }
+    }
+    echo json_encode($result);
   });
 });
 
