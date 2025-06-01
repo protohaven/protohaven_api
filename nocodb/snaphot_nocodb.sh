3e08e356aa94bb5fe066b2945358fa5a66d17492
Scott Martin <smartin015@gmail.com>
2025-06-01 12:55:34 -0400

    Fix volume naming on nocodb snapshot script

--- nocodb/snaphot_nocodb.sh
+++ nocodb/snaphot_nocodb.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Snapshotting Nocodb server container data to nc_data.tar..."
-docker run --rm --volumes-from 2_pg-nocodb-1 -v $(pwd):/backup ubuntu tar cvf /backup/nc_data.tar /usr/app/data
+docker run --rm --volumes-from nocodb-nocodb-1 -v $(pwd):/backup ubuntu tar cvf /backup/nc_data.tar /usr/app/data
 echo "Snaphsotting Nocodb postgres container to dump.sql..."
-docker exec -it 2_pg-root_db-1 pg_dump -U postgres -d root_db > dump.sql
+docker exec -it nocodb-root_db-1 pg_dump -U postgres -d root_db > dump.sql
 
 echo "Done - remember to commit nc_data.tar and dump.sql when pushing your changes!"
