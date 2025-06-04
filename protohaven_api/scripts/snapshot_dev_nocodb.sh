0e4ddcd5c5589f93efaa3cb895f6510cb13f3c23
Scott Martin <smartin015@gmail.com>
2025-06-04 07:12:33 -0400

    WIP eventbrite integration

---
+++ protohaven_api/scripts/snapshot_dev_nocodb.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Snapshotting nocodb uploads..."
+docker run --rm --volumes-from 2_pg-nocodb-1 -v $(pwd):/backup ubuntu tar cvf /backup/nc_data.tar /usr/app/data
+echo "Snapshot complete"
+
+echo "Snapshotting nocodb postgres DB..."
+docker exec -it 2_pg-root_db-1 pg_dump -U postgres -d root_db > dump.sql
+echo "Snapshot complete"
+
+
