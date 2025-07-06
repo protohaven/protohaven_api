7511f15afde4012169903c9917dc18f59c5f4463
Scott Martin <smartin015@gmail.com>
2025-07-06 10:42:44 -0400

    Respect holidays in shift forecast

--- protohaven_api/automation/techs/techs.py
+++ protohaven_api/automation/techs/techs.py
@@ -1,11 +1,20 @@
 """Forecasting methods for shop tech shift staffing"""
 import datetime
+import logging
 from collections import defaultdict
 
+import holidays
+
 from protohaven_api.integrations import airtable, neon
+from protohaven_api.integrations.models import Member
 from protohaven_api.rbac import Role
 
 DEFAULT_FORECAST_LEN = 16
+
+log = logging.getLogger("protohaven_api.automation.techs.techs")
+
+# Pylint seems to think `US()` doesn't exist. It may be dynamically loaded?
+us_holidays = holidays.US()  # pylint: disable=no-member
 
 
 def _calendar_badge_color(num_people):
@@ -19,11 +28,31 @@ def _calendar_badge_color(num_people):
     return "danger"
 
 
-def _create_calendar_view(
-    start_date, shift_map, forecast_len
+def resolve_overrides(overrides, shift):
+    """We must translate overrides into Member instances, with
+    special handling of "guest" techs if they do not exist in Neon"""
+    ovr_id, ovr_people, ovr_editor = overrides.get(shift) or (None, [], None)
+    for i, p in enumerate(ovr_people):
+        fname, lname = [n.strip() for n in p.split(" ")][:2]
+        mm = list(neon.search_members_by_name(fname, lname, also_fetch=True))
+        if len(mm) >= 1:
+            log.warning("Multiple member matches for Neon lookup of tech override {p}")
+            ovr_people[i] = mm[0]
+        else:
+            log.warning(
+                f"Tech override not found in neon: {p}. Creating name-only Member object"
+            )
+            ovr_people[i] = Member.from_neon_search(
+                {"First Name": fname, "Last Name": lname}
+            )
+    return ovr_id, ovr_people, ovr_editor
+
+
+def create_calendar_view(
+    start_date, shift_map, overrides, forecast_len
 ):  # pylint: disable=too-many-locals, too-many-nested-blocks
+    """Create a calendar view of tech shifts, respecting overrides and holidays"""
     calendar_view = []
-    overrides = dict(airtable.get_forecast_overrides())
     for i in range(forecast_len):
         d = start_date + datetime.timedelta(days=i)
         dstr = d.strftime("%Y-%m-%d")
@@ -31,19 +60,13 @@ def _create_calendar_view(
         for ap in ["AM", "PM"]:
             s = f"{d.strftime('%A')} {ap}"
 
-            ovr, ovr_people, ovr_editor = overrides.get(
-                f"{dstr} {ap}", (None, None, None)
+            ovr_id, ovr_people, ovr_editor = resolve_overrides(
+                overrides, f"{dstr} {ap}"
             )
-            for i, p in enumerate(ovr_people):
-                fname, lname = [n.strip() for n in p.split(" ")][:2]
-                mm = list(neon.search_members_by_name(fname, lname, also_fetch=True))
-                if len(mm) != 1:
-                    raise RuntimeError(
-                        "Multiple member matches for Neon lookup of tech override {p}"
-                    )
-                ovr_people[i] = mm[0]
 
-            people = shift_map.get(s, [])
+            # On holidays, we assume by default that nobody is on duty.
+            people = shift_map.get(s, []) if d not in us_holidays else []
+
             final_people = []
             for p in people:  # remove if outside of the tech's tenure
                 if (p.shop_tech_first_day is None or p.shop_tech_first_day <= d) and (
@@ -53,12 +76,16 @@ def _create_calendar_view(
 
             shift = {
                 "title": f"{d.strftime('%a %m/%d')} {ap}",
-                "people": ovr_people or final_people,
+                "people": ovr_people if ovr_id else final_people,
                 "id": f"Badge{i}{ap}",
             }
             shift["color"] = _calendar_badge_color(len(shift["people"]))
-            if ovr:
-                shift["ovr"] = {"id": ovr, "orig": final_people, "editor": ovr_editor}
+            if ovr_id:
+                shift["ovr"] = {
+                    "id": ovr_id,
+                    "orig": final_people,
+                    "editor": ovr_editor,
+                }
             day[ap] = shift
         calendar_view.append(day)
     return calendar_view
@@ -85,11 +112,12 @@ def generate(date, forecast_len, include_pii=False):
         ]
 
     date = date.replace(hour=0, minute=0, second=0, microsecond=0)
+    overrides = dict(airtable.get_forecast_overrides())
     techs = list(neon.search_members_with_role(Role.SHOP_TECH, tech_fields))
     shift_map = defaultdict(list)
     for t in techs:
         shift_map[t.shop_tech_shift].append(t)
 
     return {
-        "calendar_view": _create_calendar_view(date, shift_map, forecast_len),
+        "calendar_view": create_calendar_view(date, shift_map, overrides, forecast_len),
     }
