7b7226dfd9952aee4eb6ccf81fcffa476a9196ce
Scott Martin <smartin015@gmail.com>
2026-01-28 14:44:36 -0500

    Switch to Nocodb API v3

--- protohaven_api/integrations/airtable_base.py
+++ protohaven_api/integrations/airtable_base.py
@@ -1,6 +1,8 @@
 """Airtable basic API commands"""
 
 import logging
+from typing import Any, Iterable
+from urllib.parse import parse_qs, urlparse
 
 from protohaven_api.integrations.data.connector import get as get_connector
 
@@ -11,13 +13,30 @@ class TableNotFoundError(Exception):
     """Error for table not found"""
 
 
-def _idref(rec, field):
-    v = rec["fields"].get(field)
-    if v is None:
+def _idref(rec: dict[str, Any], field: str) -> list[str]:
+    """There's a difference in referencing IDs of linked fields in Airtable vs NocoDB.
+    This helper normalizes both and handles the null case"""
+    vv = rec["fields"].get(field)
+    if vv is None:
         return []
-    if not isinstance(v, list):
-        return [v]
-    return v
+    if isinstance(vv, dict) and "id" in vv:
+        return [str(vv["id"])]
+    if not isinstance(vv, list):
+        raise RuntimeError(
+            f"Invalid linked field data encountered for field {field}: {vv}\n"
+            "Check if field is LTAR for NocodB "
+            "(https://github.com/nocodb/nocodb/issues/12717#issuecomment-3739524647)"
+        )
+    if len(vv) > 0 and isinstance(vv[0], dict):
+        return [str(v["id"]) for v in vv]
+    return vv
+
+
+def _refid(id_ref: str) -> dict[str, int] | str:
+    """Use the correct Airtable/Nocodb format when inserting records with linked fields"""
+    if get_connector().db_format() == "nocodb":
+        return {"id": int(id_ref)}
+    return id_ref
 
 
 def get_record(base, tbl, rec):
@@ -25,22 +44,22 @@ def get_record(base, tbl, rec):
     status, content = get_connector().db_request("GET", base, tbl, rec)
     if status != 200:
         raise RuntimeError(f"Airtable fetch {base} {tbl} {rec}", status, content)
-    if get_connector().db_format() == "nocodb":
-        # Original implementation is with Airtable; NocoDB has a different response format
-        # so we massage it to look like an Airtable response.
-        return {"id": content["Id"], "fields": content}
     return content
 
 
-def get_all_records(base, tbl, suffix=None):
+MAX_ITERS = 100
+
+
+def get_all_records(
+    base: str, tbl: str, params: dict = None
+) -> Iterable[dict[str, Any]]:
     """Get all records for a given named table (ID in config.yaml)"""
-    records = []
-    offs = ""
-    while offs is not None:
-        s = f"?offset={offs}"
-        if suffix is not None:
-            s += "&" + suffix
-        status, content = get_connector().db_request("GET", base, tbl, suffix=s)
+    records: list[dict[str, Any]] = []
+    s = ""
+    params = params or {}
+    niter = 0
+    while niter < MAX_ITERS:
+        status, content = get_connector().db_request("GET", base, tbl, params=params)
         if status == 404:
             raise TableNotFoundError(
                 f"Airtable fetch {base} {tbl} {s}",
@@ -55,94 +74,81 @@ def get_all_records(base, tbl, suffix=None):
             )
         data = content
         if get_connector().db_format() == "nocodb":
-            # Original implementation is with Airtable; NocoDB has a different response format
-            # so we massage it to look like an Airtable response.
-            records += [{"id": d["Id"], "fields": d} for d in data["list"]]
-            if data["pageInfo"].get("isLastPage"):
+            records += data["records"]
+            if not "next" in data:
                 break
-            offs = data["pageInfo"]["page"] * data["pageInfo"]["pageSize"]
+            log.info(data["next"])
+
+            parsed = parse_qs(urlparse(data["next"]).query)
+            params["page"] = parsed["page"][0]
+            if "pageSize" in parsed:
+                params["pageSize"] = parsed["pageSize"][0]
         else:
             records += data["records"]
             if data.get("offset") is None:
                 break
-            offs = data["offset"]
+            params["offset"] = data["offset"]
+        niter += 1
     return records
 
 
 def get_all_records_between(base, tbl, start_date, end_date, field="Created"):
     """Returns a list of all records in the table with the
     Created field timestamp after a certain date"""
-    suffix = None
+    params = {}
     if not end_date:
         return get_all_records_after(base, tbl, start_date, field)
 
     if get_connector().db_format() == "nocodb":
-        suffix = f"where=({field},le,exactDate,{end_date.isoformat()})~and({field},ge,exactDate,{start_date.isoformat()})"  # pylint: disable=line-too-long
+        params["where"] = (
+            f"({field},le,exactDate,{end_date.isoformat()})~and({field},ge,exactDate,{start_date.isoformat()})"  # pylint: disable=line-too-long
+        )
     else:
-        suffix = f"filterByFormula=AND(IS_BEFORE(%7B{field}%7D, '{end_date.isoformat()}'), IS_AFTER(%7B{field}%7D,'{start_date.isoformat()}'))"  # pylint: disable=line-too-long
+        params["filterByFormula"] = (
+            f"AND(IS_BEFORE(%7B{field}%7D, '{end_date.isoformat()}'), IS_AFTER(%7B{field}%7D,'{start_date.isoformat()}'))"  # pylint: disable=line-too-long
+        )
 
-    return get_all_records(base, tbl, suffix=suffix)
+    return get_all_records(base, tbl, params)
 
 
 def get_all_records_after(base, tbl, after_date, field="Created"):
     """Returns a list of all records in the table with the
     Created field timestamp after a certain date"""
+    params = {}
     if get_connector().db_format() == "nocodb":
-        suffix = f"where=({field},ge,exactDate,{after_date.isoformat()})"
+        params["where"] = f"({field},ge,exactDate,{after_date.isoformat()})"
     else:
-        suffix = f"filterByFormula=IS_AFTER(%7B{field}%7D,'{after_date.isoformat()}')"
+        params["filterByFormula"] = (
+            f"IS_AFTER(%7B{field}%7D,'{after_date.isoformat()}')"
+        )
 
-    return get_all_records(base, tbl, suffix=suffix)
+    return get_all_records(base, tbl, params)
 
 
-def insert_records(records, base, tbl, link_fields=None):
+def insert_records(records, base, tbl):
     """Inserts one or more records into a named table. the "fields" structure is
     automatically applied.
-
-    Note that link_records is only needed for NocoDB, as airtable auto-links in a
-    single request.
     """
     # Max of 10 records allowed for insertion, see
     # https://airtable.com/developers/web/api/create-records
     assert len(records) <= 10
 
-    post_data = {"records": [{"fields": d} for d in records]}
-    status, content = get_connector().db_request("POST", base, tbl, data=post_data)
-
-    if get_connector().db_format() != "nocodb" or link_fields is None:
-        return status, content
-
-    log.info(f"NocoDB: handling link fields {link_fields} for records {content}...")
-    try:
-        # NocoDB requires linking as a separate request (ugh).
-        # https://github.com/nocodb/nocodb/issues/11138 tracks discussion on allowing
-        # single-request inserts with link fields.
-        for i, rec in enumerate(content):
-            for lf in link_fields or []:
-                val = records[i].get(lf)
-                if val is None:
-                    continue
-                log.info(f"Linking {lf} in {rec['Id']} to {val}")
-                if not isinstance(val, list):
-                    val = [val]
-                val = [{"Id": v} for v in val]
-                lstatus, lcontent = get_connector().db_request(
-                    "POST", base, tbl, rec["Id"], link_field=lf, data=val
-                )
-                log.info(f"Link result {lstatus}: {lcontent}")
-                assert lstatus in (201, 200)
-        return status, content
-    except Exception as e:
-        log.error("Rolling back / attempting to delete inserted records")
-        status, content = get_connector().db_request("DELETE", base, tbl, content)
-        assert status == 200
-        raise RuntimeError(f"Failed to link record {rec['Id']}, field {lf}") from e
+    post_data = [{"fields": d} for d in records]
+    if get_connector().db_format() != "nocodb":
+        post_data = {"records": post_data}
+    return get_connector().db_request("POST", base, tbl, data=post_data)
 
 
 def update_record(data, base, tbl, rec):
-    """Updates/patches a record in a named table"""
-    post_data = {"fields": data}
-    result = get_connector().db_request("PATCH", base, tbl, rec=rec, data=post_data)
+    """Updates/patches a record in a named table.
+    Note that this uses an API endpoint that supports multiple records,
+    up to a max of 10. We could modify this to better batch data requests in the future.
+    """
+    post_data = [{"id": rec, "fields": data}]
+    if get_connector().db_format() != "nocodb":
+        post_data = {"records": post_data}
+
+    result = get_connector().db_request("PATCH", base, tbl, data=post_data)
     if get_connector().db_format() != "nocodb":
         return result
 
@@ -153,23 +159,10 @@ def update_record(data, base, tbl, rec):
 
 
 def delete_record(base, tbl, rec):
-    """Deletes a record in a named table"""
-    if get_connector().db_format() == "nocodb":
-        return get_connector().db_request("DELETE", base, tbl, data=[{"Id": rec}])
-    return get_connector().db_request("DELETE", base, tbl, rec=rec)
-
-
-def link_record(
-    base: str, tbl: str, rec: int, field: str, linked_record_ids: list[int]
-):
-    """NOCODB ONLY
-    https://data-apis-v2.nocodb.com/#tag/Table-Records/operation/db-data-table-row-nested-list
+    """Deletes a record in a named table.
+    Note that this uses a NocoDB API endpoint that supports multiple records,
+    up to a max of 10. We could modify this to better batch data requests in the future.
     """
-    return get_connector().db_request(
-        "POST",
-        base,
-        tbl,
-        rec,
-        data=[{"Id": i} for i in linked_record_ids],
-        link_field=field,
-    )
+    if get_connector().db_format() == "nocodb":
+        return get_connector().db_request("DELETE", base, tbl, data=[{"id": rec}])
+    return get_connector().db_request("DELETE", base, tbl, rec=rec)
