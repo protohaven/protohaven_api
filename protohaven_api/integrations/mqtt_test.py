e1f98d8c28cea7d4e882be414f27a8db99d170e5
Scott Martin <smartin015@gmail.com>
2026-04-20 22:07:35 -0400

    Fix tests

--- protohaven_api/integrations/mqtt_test.py
+++ protohaven_api/integrations/mqtt_test.py
@@ -5,22 +5,9 @@ from protohaven_api.integrations import mqtt as m
 
 def test_on_connect(mocker):
     """Test MQTT on_connect function"""
-    c = m.Client()
+    c = m.Client(None)
     mocker.patch.object(c, "c")
     c.on_connect(None, None, {}, 0, None)
-    for topic in m.TOPICS:
-        for prefix in m.SUB_PREFIXES:
-            c.c.subscribe.assert_any_call(  # pylint: disable=no-member
-                f"{prefix}/+/{topic}"
-            )
-
-
-def test_on_shopminder_alive_msg(mocker):
-    """Test MQTT on_message function"""
-    c = m.Client()
-    mocker.patch.object(c, "c")
-    c.on_message(None, None, mocker.MagicMock(topic="prefix/minder/ALIVE", payload="1"))
-    assert c.shopminders["minder"]["alive"]
-
-    c.on_message(None, None, mocker.MagicMock(topic="prefix/minder/ALIVE", payload="0"))
-    assert not c.shopminders["minder"]["alive"]
+    c.c.subscribe.assert_any_call(  # pylint: disable=no-member
+        "/protohaven_api/v1/notify_discord"
+    )
