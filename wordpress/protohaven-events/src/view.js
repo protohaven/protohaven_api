b027f1bbd347436dc8a55266c3e0019851a3aee3
Scott Martin <smartin015@gmail.com>
2026-08-13 23:42:19 +0000

    Retry fetching events when class explorer loads empty (#616)
    * Retry fetching events when class explorer loads empty
    
    Co-authored-by: Shelley <shelley@exe.dev>
    
    * Add try/catch on event parsing
    
    ---------
    
    Co-authored-by: Shelley <shelley@exe.dev>

--- wordpress/protohaven-events/src/view.js
+++ wordpress/protohaven-events/src/view.js
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client';
 
 window.addEventListener("load", (event) => {
 	const elem = document.getElementById("protohaven-events");
-	const data = JSON.parse(elem.children[0].innerHTML);
+	let data = {};
+	try {
+		data = JSON.parse(elem.children[0].innerHTML);
+	} catch(e) {
+		console.error(e);
+	}
 	const root = createRoot(elem);
 	root.render(<App initialData={data} {...elem.dataset} />);
 });
